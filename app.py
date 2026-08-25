1cfc3aca4466c3b3cbec95fd25786185b73038ae
Art_Mainardi <iensenmainardi@gmail.com>
2026-08-25 11:28:34 -0300

    feat: adiciona rotas com parâmetros

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ def index():
 def sobre():
     return '''Esta é a página "sobre"'''
 
+@app.route("/produto/<int:id>")
+def produto(id):
+    return f"Exibindo o produto com o ID {id}"
+
+@app.route("/categoria/<nome>")
+def categoria(nome):
+    return f"Produtos da categoria: {categoria}"
+
 # Incia o servidor:
 if __name__ == "__main__":
     app.run(debug=True)
