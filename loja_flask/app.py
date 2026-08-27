058587689e7068c4b845e485e38165c30b9209e2
ArtMainardi <iensenmainardi@gmail.com>
2026-08-27 12:35:17 -0300

    feat: aula do dia 27.08

--- loja_flask/app.py
+++ loja_flask/app.py
@@ -12,13 +12,18 @@ def index():
 def sobre():
     return render_template("sobre.html")
 
+@app.route("/produto")
+def produto():
+    return render_template("produtos.html")
+
 @app.route("/produto/<int:id>")
-def produto(id):
-    render_template("produto.html")
+def buscar_produto(id):
+
+    return render_template("produtos.html", produto=produto)
 
 @app.route("/categoria/<nome>")
 def categoria(nome):
-    render_template("categoria.html")
+    return render_template("categoria.html")
 
 # Incia o servidor:
 if __name__ == "__main__":
---
+++ loja_flask/dicionario.py
@@ -0,0 +1,11 @@
+# Define uma lista de produtos para testes:
+produtos_db = [
+    {
+        "nome" : "Maçã",
+        "preco" : 2
+    },
+    {
+        "nome" : "Maçã",
+        "preco" : 2
+    }
+]
