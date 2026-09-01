351b4adc2913fbf87cc32b7ed7c655e005de44bd
Art_Mainardi <iensenmainardi@gmail.com>
2026-09-01 10:50:07 -0300

    feat: adiciona 'banco de dados' local

--- loja_flask/app.py
+++ loja_flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from config import listaProdutos
 
 # Cria a aplicação:
 app = Flask(__name__)
@@ -14,12 +15,11 @@ def sobre():
 
 @app.route("/produto")
 def produto():
-    return render_template("produtos.html")
+    return render_template("produtos.html", produtos=listaProdutos)
 
 @app.route("/produto/<int:id>")
 def buscar_produto(id):
-
-    return render_template("produtos.html", produto=produto)
+    return render_template("produtos.html", produtos=produto)
 
 @app.route("/categoria/<nome>")
 def categoria(nome):
---
+++ loja_flask/config.py
@@ -0,0 +1,23 @@
+# Cria "banco de dados":
+listaProdutos = [
+    {
+        "nome" : "Maçã",
+        "preco" : 6,
+        "categoria" : "comida"
+    },
+    {
+        "nome" : "Caderno",
+        "preco" : 30,
+        "categoria" : "educação"
+    },
+    {
+        "nome" : "Caneta",
+        "preco" : 3,
+        "categoria" : "educação"
+    },
+    {
+        "nome" : "Salgadinho de Batata",
+        "preco" : 10,
+        "categoria" : "comida"
+    }
+]
